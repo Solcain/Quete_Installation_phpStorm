7ec1b124e7a10965f531474b034ad7a56a4ed16f
Nico P <nicopinsard@gmail.com>
2017-02-27 14:34:01 +0100

    Initial commit

---
+++ Quete Installation de PhpStorm.php
@@ -0,0 +1,13 @@
+<?php
+$students = [
+    "Emmanuel" => 42,
+        "Thierry" => 51,
+        "Pascal" => 45,
+        "Eric" => 48,
+        "Nicolas" => 19
+            ];
+ * Created by PhpStorm.
+ * User: wilder8
+ * Date: 27/02/17
+ * Time: 14:26
+ */
